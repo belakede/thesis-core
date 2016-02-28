bf9a54abfe5ae643857ddc2a926bb2d6f9e63246
belakede <belakede@gmail.com>
2016-02-28 12:59:49 +0100

    Add FieldMatrix helper class
    (cherry picked from commit 6bb9f07)

---
+++ suspect-game/src/main/java/me/belakede/thesis/internal/game/board/FieldMatrix.java
@@ -0,0 +1,111 @@
+package me.belakede.thesis.internal.game.board;
+
+import me.belakede.thesis.game.board.Field;
+import me.belakede.thesis.game.board.FieldType;
+import me.belakede.thesis.game.board.RoomField;
+
+import java.io.Serializable;
+import java.util.*;
+
+final class FieldMatrix implements Serializable {
+
+    public static final int MAX_DISTANCE = 12;
+
+    private final int size;
+    private final Field[][] fields;
+    private transient List<Field> roomFields;
+    private transient Set<Field> startingFields;
+    private transient Map<Field, IndexPair> indices;
+
+    public FieldMatrix(FieldMatrix another) {
+        this(another.size, another.fields);
+    }
+
+    public FieldMatrix(int size, Field[][] fields) {
+        this.size = size;
+        this.fields = fields;
+        this.indices = new HashMap<>();
+        this.roomFields = new ArrayList<>();
+        this.startingFields = new HashSet<>();
+        uploadHelperFields();
+    }
+
+    public List<Field> getRoomFields() {
+        checkHelpers();
+        return new ArrayList<>(roomFields);
+    }
+
+    private void checkHelpers() {
+        if (indices == null) {
+            uploadHelperFields();
+        }
+    }
+
+    public Set<Field> getStartingFields() {
+        checkHelpers();
+        return new HashSet<>(startingFields);
+    }
+
+    public boolean isAvailable(Field from, Field to) {
+        checkHelpers();
+        return indices.get(from).getDistance(indices.get(to)) <= MAX_DISTANCE;
+    }
+
+    public Set<Field> getAvailableFields(Field from, int step) {
+        checkHelpers();
+        Set<Field> availableFields = new HashSet<>();
+        IndexPair indexPair = indices.get(from);
+        int startRowIndex = Math.max(indexPair.getRow() - step - 1, 0);
+        int startColumnIndex = Math.max(indexPair.getColumn() - step - 1, 0);
+        int endRowIndex = Math.min(indexPair.getRow() + step + 1, size);
+        int endColumnIndex = Math.min(indexPair.getColumn() + step + 1, size);
+        for (int i = startRowIndex; i < endRowIndex; i++) {
+            for (int j = startColumnIndex; j < endColumnIndex; j++) {
+                if (indexPair.getDistance(i, j) <= step) {
+                    availableFields.add(fields[i][j]);
+                }
+            }
+        }
+        return availableFields;
+    }
+
+    private void uploadHelperFields() {
+        for (int i = 0; i < fields.length; i++) {
+            for (int j = 0; j < fields[i].length; j++) {
+                indices.put(fields[i][j], new IndexPair(i, j));
+                if (FieldType.START.equals(fields[i][j].getType())) {
+                    startingFields.add(fields[i][j]);
+                } else if (fields[i][j] instanceof RoomField) {
+                    roomFields.add(fields[i][j]);
+                }
+            }
+        }
+    }
+
+    private static final class IndexPair {
+        private final int row;
+        private final int column;
+
+        public IndexPair(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getColumn() {
+            return column;
+        }
+
+        public int getDistance(IndexPair other) {
+            return Math.abs(row - other.row) + Math.abs(column - other.column);
+        }
+
+        public int getDistance(int otherRow, int otherColumn) {
+            return Math.abs(row - otherRow) + Math.abs(column - otherColumn);
+        }
+    }
+
+}
---
+++ suspect-game/src/test/java/me/belakede/thesis/internal/game/board/FieldMatrixTest.java
@@ -0,0 +1,167 @@
+package me.belakede.thesis.internal.game.board;
+
+import me.belakede.thesis.game.board.Field;
+import me.belakede.thesis.game.board.FieldType;
+import me.belakede.thesis.game.board.RoomField;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.*;
+
+
+public class FieldMatrixTest {
+
+    @Test
+    public void testIsAvailableShouldReturnTrueWhenToFieldDistanceIsLowerThanTwelve() throws Exception {
+        // GIVEN
+        Field[][] matrix = getFieldMatrix(7);
+        // WHEN
+        FieldMatrix testSubject = new FieldMatrix(7, matrix);
+        Field from = matrix[0][0];
+        Field to = matrix[6][6];
+        // THEN
+        boolean actual = testSubject.isAvailable(from, to);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void testIsAvailableShouldReturnTrueWhenToFieldEqualsFromField() throws Exception {
+        // GIVEN
+        Field[][] matrix = getFieldMatrix(1);
+        // WHEN
+        FieldMatrix testSubject = new FieldMatrix(1, matrix);
+        Field from = matrix[0][0];
+        Field to = matrix[0][0];
+        // THEN
+        boolean actual = testSubject.isAvailable(from, to);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void testIsAvailableShouldReturnFalseWhenToFieldDistanceIsGreaterThanTwelve() throws Exception {
+        // GIVEN
+        Field[][] matrix = getFieldMatrix(8);
+        // WHEN
+        FieldMatrix testSubject = new FieldMatrix(8, matrix);
+        Field from = matrix[0][0];
+        Field to = matrix[7][6];
+        // THEN
+        boolean actual = testSubject.isAvailable(from, to);
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void testGetAvailableFieldsShouldReturnSetOfNeighbourFieldsWhenStepIsOne() throws Exception {
+        // GIVEN
+        Field[][] matrix = getFieldMatrix(3);
+        // WHEN
+        FieldMatrix testSubject = new FieldMatrix(3, matrix);
+        int step = 1;
+        Field from = matrix[1][1];
+        // THEN
+        Set<Field> expected = new HashSet<>(Arrays.asList(matrix[0][1], matrix[1][0], matrix[1][1], matrix[1][2], matrix[2][1]));
+        Set<Field> actual = testSubject.getAvailableFields(from, step);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetAvailableFieldsShouldReturnAllFieldsWhenFromIsTheCenterOfAThreeTimesThreeMatrixAndStepIsTwo() throws Exception {
+        // GIVEN
+        Field[][] matrix = getFieldMatrix(3);
+        // WHEN
+        FieldMatrix testSubject = new FieldMatrix(3, matrix);
+        int step = 2;
+        Field from = matrix[1][1];
+        // THEN
+        Set<Field> expected = new HashSet<>(Arrays.asList(
+                matrix[0][0], matrix[0][1], matrix[0][2],
+                matrix[1][0], matrix[1][1], matrix[1][2],
+                matrix[2][0], matrix[2][1], matrix[2][2]));
+        Set<Field> actual = testSubject.getAvailableFields(from, step);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetRoomFieldsShouldReturnTheOnlyOneRoomFieldOnBoard() throws Exception {
+        // GIVEN
+        RoomField roomField = Mockito.mock(RoomField.class);
+        Field[][] matrix = getFieldMatrix(3);
+        // WHEN
+        matrix[1][1] = roomField;
+        FieldMatrix testSubject = new FieldMatrix(3, matrix);
+        // THEN
+        List<Field> expected = Arrays.asList(roomField);
+        List<Field> actual = testSubject.getRoomFields();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetRoomFieldsShouldReturnAllRoomFieldsOnBoard() throws Exception {
+        // GIVEN
+        RoomField firstRoomField = Mockito.mock(RoomField.class);
+        RoomField secondRoomField = Mockito.mock(RoomField.class);
+        RoomField thirdRoomField = Mockito.mock(RoomField.class);
+        RoomField fourthRoomField = Mockito.mock(RoomField.class);
+        Field[][] matrix = getFieldMatrix(5);
+        // WHEN
+        matrix[1][0] = firstRoomField;
+        matrix[3][2] = secondRoomField;
+        matrix[3][3] = thirdRoomField;
+        matrix[4][4] = fourthRoomField;
+        FieldMatrix testSubject = new FieldMatrix(3, matrix);
+        // THEN
+        List<Field> expected = Arrays.asList(firstRoomField, secondRoomField, thirdRoomField, fourthRoomField);
+        List<Field> actual = testSubject.getRoomFields();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetStartingFieldsShouldReturnTheOnlyOneStartingFieldOnBoard() throws Exception {
+        // GIVEN
+        Field[][] matrix = getFieldMatrix(2);
+        Field field = matrix[1][1];
+        // WHEN
+        Mockito.when(field.getType()).thenReturn(FieldType.START);
+        FieldMatrix testSubject = new FieldMatrix(3, matrix);
+        // THEN
+        Mockito.verify(field).getType();
+        Set<Field> expected = new HashSet<>(Arrays.asList(field));
+        Set<Field> actual = testSubject.getStartingFields();
+
+        Assert.assertEquals(1, actual.size());
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetStartingFieldsShouldReturnAllStartingFieldsOnBoard() throws Exception {
+        // GIVEN
+        Field[][] matrix = getFieldMatrix(2);
+        Field firstField = matrix[0][0];
+        Field secondField = matrix[1][1];
+        // WHEN
+        Mockito.when(firstField.getType()).thenReturn(FieldType.START);
+        Mockito.when(secondField.getType()).thenReturn(FieldType.START);
+        FieldMatrix testSubject = new FieldMatrix(3, matrix);
+        // THEN
+        Mockito.verify(firstField).getType();
+        Mockito.verify(secondField).getType();
+        Set<Field> expected = new HashSet<>(Arrays.asList(firstField, secondField));
+        Set<Field> actual = testSubject.getStartingFields();
+
+        Assert.assertEquals(2, actual.size());
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    private Field[][] getFieldMatrix(int size) {
+        Field[][] matrix = new Field[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = Mockito.mock(Field.class);
+            }
+        }
+        return matrix;
+    }
+
+}

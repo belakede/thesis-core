5d359e054fa7e5562b15cc4e73d0ef67b8f7e064
Adam Belak <abelak@hortonworks.com>
2016-03-09 20:55:50 +0100

    Implement Player
    (cherry picked from commit 801eee3)

--- suspect-game/src/main/java/me/belakede/thesis/game/Player.java
+++ suspect-game/src/main/java/me/belakede/thesis/game/Player.java
@@ -3,11 +3,15 @@ package me.belakede.thesis.game;
 import me.belakede.thesis.game.equipment.Card;
 import me.belakede.thesis.game.equipment.Suspect;
 
+import java.util.Set;
+
 public interface Player {
 
     void makeGroundlessAccusation();
 
     boolean hasBeenMadeGroundlessAccusation();
+
+    Set<Card> getCards();
 
     boolean hasCard(Card card);
 
---
+++ suspect-game/src/main/java/me/belakede/thesis/internal/game/DefaultPlayer.java
@@ -0,0 +1,75 @@
+package me.belakede.thesis.internal.game;
+
+
+import me.belakede.thesis.game.Player;
+import me.belakede.thesis.game.equipment.Card;
+import me.belakede.thesis.game.equipment.Suspect;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class DefaultPlayer implements Player {
+
+    private final Set<Card> cards;
+    private final Suspect figurine;
+    private boolean madeGroundlessAccusation;
+
+    public DefaultPlayer(Suspect figurine, Collection<Card> cards) {
+        this.figurine = figurine;
+        this.cards = new HashSet<>(cards);
+        this.madeGroundlessAccusation = false;
+    }
+
+    @Override
+    public void makeGroundlessAccusation() {
+        madeGroundlessAccusation = true;
+    }
+
+    @Override
+    public boolean hasBeenMadeGroundlessAccusation() {
+        return madeGroundlessAccusation;
+    }
+
+    @Override
+    public Set<Card> getCards() {
+        return new HashSet<>(cards);
+    }
+
+    @Override
+    public boolean hasCard(Card card) {
+        return cards.contains(card);
+    }
+
+    @Override
+    public Suspect getFigurine() {
+        return figurine;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DefaultPlayer that = (DefaultPlayer) o;
+
+        if (!cards.equals(that.cards)) return false;
+        return figurine == that.figurine;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = cards.hashCode();
+        result = 31 * result + figurine.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "DefaultPlayer{" +
+                "figurine=" + figurine +
+                ", madeGroundlessAccusation=" + madeGroundlessAccusation +
+                '}';
+    }
+}

51c3693b51fd8d9aba0682062c2878e778c98a98
Adam Belak <abelak@hortonworks.com>
2016-03-11 22:02:24 +0100

    Add getPositionMap() and isGameEnded()  to Game interface
    (cherry picked from commit f02d981)

--- suspect-game/src/main/java/me/belakede/thesis/game/Game.java
+++ suspect-game/src/main/java/me/belakede/thesis/game/Game.java
@@ -2,12 +2,17 @@ package me.belakede.thesis.game;
 
 import me.belakede.thesis.game.board.Board;
 import me.belakede.thesis.game.board.Field;
+import me.belakede.thesis.game.equipment.Figurine;
 import me.belakede.thesis.game.equipment.PairOfDice;
 import me.belakede.thesis.game.equipment.Suspicion;
+
+import java.util.Map;
 
 public interface Game {
 
     Board getBoard();
+
+    Map<Figurine, Field> getPositions();
 
     PairOfDice roll();
 
@@ -17,4 +22,5 @@ public interface Game {
 
     void accuse(Suspicion suspicion);
 
+    boolean isGameEnded();
 }

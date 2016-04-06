1b9d43a96cc33297f2f6b6197bc0678a76b82e0c
Adam Belak <abelak@hortonworks.com>
2016-04-06 15:58:52 +0200

    Add class to load board from files
    (cherry picked from commit 9a9857c)

---
+++ suspect-game/src/main/java/me/belakede/thesis/internal/game/importer/MapLoader.java
@@ -0,0 +1,57 @@
+package me.belakede.thesis.internal.game.importer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.belakede.thesis.game.board.Board;
+import me.belakede.thesis.game.board.BoardType;
+import me.belakede.thesis.game.board.Field;
+import me.belakede.thesis.internal.game.board.DefaultBoard;
+import me.belakede.thesis.internal.game.board.FieldFactory;
+import me.belakede.thesis.internal.game.importer.domain.RoomDescription;
+import me.belakede.thesis.internal.game.importer.transformer.DescriptorTransformer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class MapLoader {
+
+    private static final String BOARD_PATH = "game/board/";
+    private static final String MAP_EXTENSION = ".map";
+    private static final String ROOM_DESCRIPTOR_EXTENSION = ".rdesc";
+
+    private final BoardType boardType;
+
+
+    public MapLoader(BoardType boardType) {
+        this.boardType = boardType;
+    }
+
+    public Board load() throws IOException {
+        Field[][] fields = loadFields();
+        RoomDescription roomDescription = loadRoomDescription();
+        DescriptorTransformer transformer = new DescriptorTransformer(fields, roomDescription);
+        return new DefaultBoard(boardType, fields, transformer.getRoomFields(), transformer.getSecretPassages());
+    }
+
+    private Field[][] loadFields() {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(BOARD_PATH + boardType.toString() + MAP_EXTENSION);
+        Field[][] fields = new Field[boardType.getSize()][boardType.getSize()];
+        Scanner scanner = new Scanner(inputStream);
+        int row = 0;
+        while (scanner.hasNextLine()) {
+            char[] chars = scanner.nextLine().toCharArray();
+            for (int column = 0; column < chars.length; column++) {
+                fields[row][column] = FieldFactory.getFieldBySymbol(row, column, chars[column]);
+            }
+            row = row + 1;
+        }
+        return fields;
+    }
+
+    private RoomDescription loadRoomDescription() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(BOARD_PATH + boardType.toString() + ROOM_DESCRIPTOR_EXTENSION);
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(inputStream, RoomDescription.class);
+    }
+
+}
---
+++ suspect-game/src/test/java/me/belakede/thesis/internal/game/importer/MapLoaderTest.java
@@ -0,0 +1,84 @@
+package me.belakede.thesis.internal.game.importer;
+
+import me.belakede.thesis.game.board.Board;
+import me.belakede.thesis.game.board.BoardType;
+import me.belakede.thesis.game.board.Field;
+import me.belakede.thesis.game.board.RoomField;
+import me.belakede.thesis.game.equipment.Room;
+import me.belakede.thesis.internal.game.board.DefaultRoomField;
+import me.belakede.thesis.internal.game.board.FieldFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MapLoaderTest {
+
+    private MapLoader testSubject;
+
+    @Before
+    public void setUp() throws Exception {
+        testSubject = new MapLoader(BoardType.DEFAULT);
+    }
+
+    @Test
+    public void testLoadDefaultShouldReturnABoardWithNineRoomsAndSixStartingFields() throws Exception {
+        Board board = testSubject.load();
+
+        assertEquals(9, board.getRoomFields().size());
+        assertEquals(6, board.getStartingFields().size());
+    }
+
+    @Test
+    public void testLoadDefaultShouldReturnABoardWhichContainsWorkroom() throws Exception {
+        RoomField workroom = getWorkroom();
+        Set<RoomField> actual = testSubject.load().getRoomFields();
+        assertTrue(actual.contains(workroom));
+    }
+
+    @Test
+    public void testLoadDefaultShouldReturnABoardWithTheSpecifiedWorkroom() throws Exception {
+        RoomField workroom = getWorkroom();
+        Set<RoomField> actual = testSubject.load().getRoomFields();
+        assertTrue(actual.contains(workroom));
+        assertEquals(workroom, actual.stream().filter(rf -> Room.WORKROOM.equals(rf.getRoom())).findFirst().get());
+    }
+
+    private RoomField getWorkroom() {
+        return new DefaultRoomField(Room.WORKROOM, getWorkroomFields(), getWorkroomExitFields());
+    }
+
+    private Set<Field> getWorkroomFields() {
+        return new HashSet<>(Arrays.asList(
+                FieldFactory.getFieldBySymbol(21, 11, 'R'), FieldFactory.getFieldBySymbol(21, 12, 'R'),
+                FieldFactory.getFieldBySymbol(21, 13, 'R'), FieldFactory.getFieldBySymbol(21, 14, 'R'),
+                FieldFactory.getFieldBySymbol(21, 15, 'R'),
+                FieldFactory.getFieldBySymbol(22, 11, 'R'), FieldFactory.getFieldBySymbol(22, 12, 'R'),
+                FieldFactory.getFieldBySymbol(22, 13, 'R'), FieldFactory.getFieldBySymbol(22, 14, 'R'),
+                FieldFactory.getFieldBySymbol(22, 15, 'R'),
+                FieldFactory.getFieldBySymbol(23, 11, 'R'), FieldFactory.getFieldBySymbol(23, 12, 'R'),
+                FieldFactory.getFieldBySymbol(23, 13, 'R'), FieldFactory.getFieldBySymbol(23, 14, 'R'),
+                FieldFactory.getFieldBySymbol(23, 15, 'R'),
+                FieldFactory.getFieldBySymbol(24, 11, 'R'), FieldFactory.getFieldBySymbol(24, 12, 'R'),
+                FieldFactory.getFieldBySymbol(24, 13, 'R'), FieldFactory.getFieldBySymbol(24, 14, 'R'),
+                FieldFactory.getFieldBySymbol(24, 15, 'R'),
+                FieldFactory.getFieldBySymbol(25, 11, 'R'), FieldFactory.getFieldBySymbol(25, 12, 'R'),
+                FieldFactory.getFieldBySymbol(25, 13, 'R'), FieldFactory.getFieldBySymbol(25, 14, 'R'),
+                FieldFactory.getFieldBySymbol(25, 15, 'R')
+        ));
+    }
+
+    private HashSet<Field> getWorkroomExitFields() {
+        return new HashSet<>(Arrays.asList(
+                FieldFactory.getFieldBySymbol(20, 11, 'R'),
+                FieldFactory.getFieldBySymbol(20, 12, 'R'),
+                FieldFactory.getFieldBySymbol(20, 14, 'R'),
+                FieldFactory.getFieldBySymbol(20, 15, 'R')
+        ));
+    }
+}

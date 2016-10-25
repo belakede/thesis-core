7bc4730a3cf3953caf421e80c63e94f7814067eb
Adam Belak <belakede@gmail.com>
2016-10-25 09:34:31 +0200

    Improve DefaultSuspicionTest cases

--- suspect-equipments/src/test/java/me/belakede/thesis/internal/game/equipment/DefaultSuspicionTest.java
+++ suspect-equipments/src/test/java/me/belakede/thesis/internal/game/equipment/DefaultSuspicionTest.java
@@ -1,64 +1,28 @@
 package me.belakede.thesis.internal.game.equipment;
 
+import junitx.extensions.EqualsHashCodeTestCase;
 import me.belakede.thesis.game.equipment.*;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
-
-
+@RunWith(Enclosed.class)
 public class DefaultSuspicionTest {
 
-    private Suspicion testSubject;
+    public static final class DefaultSuspicionEqualsHashCodeTest extends EqualsHashCodeTestCase {
 
-    @Before
-    public void setUp() throws Exception {
-        testSubject = new DefaultSuspicion(Suspect.GREEN, Room.BILLIARD_ROOM, Weapon.CANDLESTICK);
+        public DefaultSuspicionEqualsHashCodeTest(String name) {
+            super(name);
+        }
+
+        @Override
+        protected Suspicion createInstance() throws Exception {
+            return new DefaultSuspicion(Suspect.GREEN, Room.BILLIARD_ROOM, Weapon.CANDLESTICK);
+        }
+
+        @Override
+        protected Suspicion createNotEqualInstance() throws Exception {
+            return new DefaultSuspicion(Suspect.WHITE, Room.BILLIARD_ROOM, Weapon.CANDLESTICK);
+        }
     }
 
-    @Test
-    public void testEqualsShouldReturnTrueWhenArgumentIsSame() throws Exception {
-        assertTrue(testSubject.equals(testSubject));
-    }
-
-    @Test
-    public void testEqualsShouldReturnFalseWhenArgumentIsNull() throws Exception {
-        assertFalse(testSubject.equals(null));
-    }
-
-    @Test
-    public void testEqualsShouldReturnFalseWhenArgumentIsDifferentType() throws Exception {
-        assertFalse(testSubject.equals(Mockito.mock(Case.class)));
-    }
-
-    @Test
-    public void testEqualsShouldReturnFalseWhenArgumentIsSuspicionButSuspectIsDifferent() throws Exception {
-        Suspicion suspicion = new DefaultSuspicion(Suspect.MUSTARD, Room.BILLIARD_ROOM, Weapon.CANDLESTICK);
-        assertFalse(testSubject.equals(suspicion));
-    }
-
-    @Test
-    public void testEqualsShouldReturnFalseWhenArgumentIsSuspicionButRoomIsDifferent() throws Exception {
-        Suspicion suspicion = new DefaultSuspicion(Suspect.GREEN, Room.DINING_ROOM, Weapon.CANDLESTICK);
-        assertFalse(testSubject.equals(suspicion));
-    }
-
-    @Test
-    public void testEqualsShouldReturnFalseWhenArgumentIsSuspicionButWeaponIsDifferent() throws Exception {
-        Suspicion suspicion = new DefaultSuspicion(Suspect.GREEN, Room.BILLIARD_ROOM, Weapon.KNIFE);
-        assertFalse(testSubject.equals(suspicion));
-    }
-
-    @Test
-    public void testEqualsShouldReturnTrueWhenArgumentIsSimilarSuspicion() throws Exception {
-        Suspicion suspicion = new DefaultSuspicion(Suspect.GREEN, Room.BILLIARD_ROOM, Weapon.CANDLESTICK);
-        assertTrue(testSubject.equals(suspicion));
-    }
-
-    @Test
-    public void testHashCodeShouldReturnTheSameValueWhenArgumentIsSimilarSuspicion() throws Exception {
-        Suspicion suspicion = new DefaultSuspicion(Suspect.GREEN, Room.BILLIARD_ROOM, Weapon.CANDLESTICK);
-        assertEquals(testSubject.hashCode(), suspicion.hashCode());
-    }
 }

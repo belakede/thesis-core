7621f4f1de34ee2c596b18793aac839a9a3412ae
Adam Belak <belakede@gmail.com>
2016-10-25 10:31:15 +0200

    Improve DefaultFieldTest

--- suspect-game/src/test/java/me/belakede/thesis/internal/game/board/DefaultFieldTest.java
+++ suspect-game/src/test/java/me/belakede/thesis/internal/game/board/DefaultFieldTest.java
@@ -67,51 +67,5 @@ public class DefaultFieldTest {
             assertFalse(testSubject.canMakeAnAccusation());
         }
 
-        @Test
-        public void testEqualsShouldReturnTrueWhenArgumentIsSimilar() throws Exception {
-            Field testSubject = new DefaultField(FieldType.ROOM, 1, 1);
-            Field copyOfTestSubject = new DefaultField(FieldType.ROOM, 1, 1);
-            assertTrue(testSubject.equals(copyOfTestSubject));
-            assertTrue(copyOfTestSubject.equals(testSubject));
-        }
-
-        @Test
-        public void testEqualsShouldReturnTrueWhenArgumentIsSame() throws Exception {
-            Field testSubject = new DefaultField(FieldType.ROOM, 1, 1);
-            assertTrue(testSubject.equals(testSubject));
-        }
-
-        @Test
-        public void testEqualsShouldReturnFalseWhenArgumentIsDifferent() throws Exception {
-            Field testSubject = new DefaultField(FieldType.ROOM, 1, 1);
-            Field anotherField = new DefaultField(FieldType.SIMPLE, 4, 2);
-            assertFalse(testSubject.equals(anotherField));
-        }
-
-        @Test
-        public void testEqualsShouldReturnFalseWhenArgumentIsNull() throws Exception {
-            Field testSubject = new DefaultField(FieldType.ROOM, 1, 1);
-            assertFalse(testSubject.equals(null));
-        }
-
-        @Test
-        public void testHashCodeShouldReturnTheSameHashCodeWhenArgumentIsSimilar() throws Exception {
-            Field testSubject = new DefaultField(FieldType.ROOM, 1, 1);
-            Field copyOfTestSubject = new DefaultField(FieldType.ROOM, 1, 1);
-            assertEquals(testSubject.hashCode(), copyOfTestSubject.hashCode());
-        }
-
-        @Test
-        public void testHashCodeShouldReturnTheSameHashCodeWhenArgumentIsSame() throws Exception {
-            Field testSubject = new DefaultField(FieldType.ROOM, 1, 1);
-            assertEquals(testSubject.hashCode(), testSubject.hashCode());
-        }
-
-        @Test
-        public void testHashCodeShouldReturnDifferentValuesWhenArgumentIsDifferent() throws Exception {
-            Field testSubject = new DefaultField(FieldType.ROOM, 1, 1);
-            Field anotherField = new DefaultField(FieldType.SIMPLE, 4, 2);
-            assertNotEquals(testSubject.hashCode(), anotherField.hashCode());
-        }
     }
 }

3c35036c296aaffdef08906860655a180ad2179a
Adam Belak <belakede@gmail.com>
2016-10-28 14:29:51 +0200

    Add NoFieldShadowingExceptSerialVersionUID rule

---
+++ suspect-test/src/main/java/me/belakede/thesis/junit/me/belakede/thesis/openpojo/rule/NoFieldShadowingExceptSerialVersionUID.java
@@ -0,0 +1,31 @@
+package me.belakede.thesis.junit.me.belakede.thesis.openpojo.rule;
+
+import com.openpojo.log.utils.MessageFormatter;
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.validation.affirm.Affirm;
+import com.openpojo.validation.rule.Rule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoFieldShadowingExceptSerialVersionUID implements Rule {
+
+    @Override
+    public void evaluate(PojoClass pojoClass) {
+        PojoClass parentPojoClass;
+        List<String> parentFields = new ArrayList<>();
+
+        parentPojoClass = pojoClass.getSuperClass();
+        do {
+            parentPojoClass.getPojoFields().stream()
+                    .filter(pf -> !(pf.isPrivate() && pf.isStatic() && pf.isFinal() && "serialVersionUID".equalsIgnoreCase(pf.getName())))
+                    .map(pf -> pf.getName())
+                    .forEach(parentFields::add);
+            parentPojoClass = parentPojoClass.getSuperClass();
+        } while (parentPojoClass != null);
+        pojoClass.getPojoFields().stream().filter(pf -> parentFields.contains(pf.getName())).forEach(pf -> {
+            Affirm.fail(MessageFormatter.format("Field=[{0}] shadows field with the same name in parent class=[{1}]", new Object[]{pf, parentFields}));
+        });
+    }
+
+}

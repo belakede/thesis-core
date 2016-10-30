3a93698e2d9dc667f73e3af05590e8a71ca5a093
belakede <belakede@gmail.com>
2016-10-30 19:27:02 +0100

    Add GameRebuilder to build a game for a specified state

---
+++ suspect-game/src/main/java/me/belakede/thesis/internal/game/util/GameRebuilder.java
@@ -0,0 +1,93 @@
+package me.belakede.thesis.internal.game.util;
+
+import me.belakede.thesis.game.Game;
+import me.belakede.thesis.game.Player;
+import me.belakede.thesis.game.board.Board;
+import me.belakede.thesis.game.board.BoardType;
+import me.belakede.thesis.game.board.Field;
+import me.belakede.thesis.game.equipment.Case;
+import me.belakede.thesis.game.equipment.Figurine;
+import me.belakede.thesis.game.equipment.Suspicion;
+import me.belakede.thesis.internal.game.DefaultGame;
+import me.belakede.thesis.internal.game.PlayerCycle;
+import me.belakede.thesis.internal.game.equipment.DefaultCase;
+
+import java.io.IOException;
+import java.util.*;
+
+public class GameRebuilder {
+
+    public static BoardStep create() {
+        return new GameSteps();
+    }
+
+    @FunctionalInterface
+    public interface BoardStep {
+        CaseStep boardType(BoardType boardType) throws IOException;
+    }
+
+    @FunctionalInterface
+    public interface CaseStep {
+        PlayersStep mystery(Suspicion suspicion);
+    }
+
+    @FunctionalInterface
+    public interface PlayersStep {
+        PositionsStep players(List<Player> orderedPlayers, Player currentPlayer);
+    }
+
+    @FunctionalInterface
+    public interface PositionsStep {
+        BuildStep positions(Map<Figurine, Coordinate> knownPositions);
+    }
+
+    @FunctionalInterface
+    public interface BuildStep {
+        Game build();
+    }
+
+    public static final class GameSteps implements BoardStep, CaseStep, PlayersStep, PositionsStep, BuildStep {
+
+        private Board board;
+        private Case mystery;
+        private PlayerCycle players;
+        private Map<Figurine, Field> positions;
+
+        @Override
+        public CaseStep boardType(BoardType boardType) throws IOException {
+            board = Boards.getBoardByType(boardType);
+            return this;
+        }
+
+        @Override
+        public PlayersStep mystery(Suspicion suspicion) {
+            mystery = new DefaultCase(suspicion);
+            return this;
+        }
+
+        @Override
+        public PositionsStep players(List<Player> orderedPlayers, Player currentPlayer) {
+            players = Players.createOrderedPlayerCycle(orderedPlayers);
+            while (!currentPlayer.equals(players.getCurrent())) {
+                players.next();
+            }
+            return this;
+        }
+
+        @Override
+        public BuildStep positions(Map<Figurine, Coordinate> knownPositions) {
+            positions = Figurines.startingPositions(board);
+            Set<Map.Entry<Figurine, Coordinate>> entries = new HashSet<>(knownPositions.entrySet());
+            entries.forEach(e -> positions.put(e.getKey(), board.getField(e.getValue().getRow(), e.getValue().getColumn())));
+            return this;
+        }
+
+        @Override
+        public Game build() {
+            return new DefaultGame(board, players, positions);
+        }
+
+
+    }
+
+}
---
+++ suspect-game/src/test/java/me/belakede/thesis/internal/game/util/GameRebuilderIntegrationTest.java
@@ -0,0 +1,52 @@
+package me.belakede.thesis.internal.game.util;
+
+import me.belakede.thesis.game.Game;
+import me.belakede.thesis.game.Player;
+import me.belakede.thesis.game.board.BoardType;
+import me.belakede.thesis.game.equipment.*;
+import me.belakede.thesis.internal.game.DefaultPlayer;
+import me.belakede.thesis.internal.game.equipment.DefaultCase;
+import me.belakede.thesis.internal.game.equipment.DefaultSuspicion;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.*;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+
+public class GameRebuilderIntegrationTest {
+
+    @Test
+    public void testCanCreateAValidDefaultGameWithFourPlayer() throws IOException {
+        Suspicion suspicion = new DefaultSuspicion(Suspect.PEACOCK, Room.DINING_ROOM, Weapon.WRENCH);
+        List<Coordinate> coordinates = new ArrayList<>(Arrays.asList(new Coordinate(2, 2),
+                new Coordinate(6, 6), new Coordinate(10, 10), new Coordinate(23, 3)));
+        List<Player> playerList = new ArrayList<>();
+        Map<Figurine, Coordinate> knownPositions = new HashMap<>();
+        Map<Figurine, Set<Card>> figurinesCards = Figurines.cards(4, new DefaultCase(suspicion));
+        figurinesCards.entrySet().forEach(e -> {
+            playerList.add(new DefaultPlayer((Suspect) e.getKey(), e.getValue()));
+            knownPositions.put(e.getKey(), coordinates.get(0));
+            coordinates.remove(0);
+        });
+        Player currentPlayer = playerList.get(2);
+        Game actual = GameRebuilder.create()
+                .boardType(BoardType.DEFAULT)
+                .mystery(suspicion)
+                .players(playerList, currentPlayer)
+                .positions(knownPositions)
+                .build();
+
+        assertThat(actual.getBoard().getBoardType(), is(BoardType.DEFAULT));
+        assertThat(actual.getPositions().size(), is(Figurines.getNumberOfFigurines()));
+        assertThat(actual.getCurrentPlayer(), is(currentPlayer));
+        knownPositions.keySet().forEach(f -> {
+            assertThat(actual.getPositions().get(f).getColumn(), is(knownPositions.get(f).getColumn()));
+            assertThat(actual.getPositions().get(f).getRow(), is(knownPositions.get(f).getRow()));
+        });
+    }
+
+}
